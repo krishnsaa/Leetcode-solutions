2fd44af99102d0e4eaf7c061df0438f466fbfa06
Krishan Pal Singh Sisodiya <153299767+krishnsaa@users.noreply.github.com>
2026-05-15 22:22:41 +0530

    dsa

--- Best_time_stock.java
+++ Best_time_stock.java
@@ -5,9 +5,10 @@ public class Best_time_stock {
     for (int i = 0; i < prices.length; i++) {
         minPrice = Math.min(minPrice, prices[i]);
         maxPro = Math.max(maxPro, prices[i] - minPrice);
+        
     }
     return maxPro;
-    
+
     }
 
 
