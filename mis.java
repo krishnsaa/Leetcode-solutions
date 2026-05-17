234ab3af334ce5281cbad12f40f7a7107fb8599c
Krishan Pal Singh Sisodiya <153299767+krishnsaa@users.noreply.github.com>
2026-05-17 22:18:25 +0530

    jv

---
+++ mis.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class mis {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=i){
+                return i;
+            }
+        }
+        return n;
+        
+    }
+}
