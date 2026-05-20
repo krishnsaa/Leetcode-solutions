6f3c32cb0622aa77f47b89cc76598a275f55a2bb
Krishan Pal Singh Sisodiya <153299767+krishnsaa@users.noreply.github.com>
2026-05-20 20:13:04 +0530

    added

--- prefix_sum_freq.java
+++ prefix_sum_freq.java
@@ -1,3 +1,20 @@
+import java.util.Arrays;
+
 public class prefix_sum_freq {
-    
+    public int[] findThePrefixCommonArray(int[] A, int[] B) {
+        int n=A.length;
+        int[] result=new int[n];
+        int[] freq=new int[n+1];
+        Arrays.fill(freq,0);
+        int count=0;
+        for(int i=0;i<n;i++){
+            freq[A[i]]++;
+            count+=freq[A[i]]==2 ? 1 :0;
+            freq[B[i]]++;
+            count+=freq[B[i]]==2 ? 1 :0;
+            result[i]=count;
+            
+        }
+        return result;
+    }
 }
