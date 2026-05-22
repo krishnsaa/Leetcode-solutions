1ee5d466963b790d5e3d0bb00f63201b2651c631
Krishan Pal Singh Sisodiya <153299767+krishnsaa@users.noreply.github.com>
2026-05-22 13:43:00 +0530

    jump

---
+++ jump2.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class jump2 {
+    public boolean canReach(int[] arr, int start) {
+        int n=arr.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp,0);
+        return support(start,arr,dp);
+    }
+    public boolean support(int i,int[] arr,int[] dp){
+        int n=arr.length;
+        if(i>=n || i<0 || dp[i]==1) {
+            return false;
+        }
+        dp[i]=1;
+        if(arr[i]==0) return true;
+        return support(i-arr[i],arr,dp) || support(i+arr[i],arr,dp);
+    }
+}
