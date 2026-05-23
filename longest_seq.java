7ef6da81fe6deccf15719cdd9c74eefe178c60ed
Krishan Pal Singh Sisodiya <153299767+krishnsaa@users.noreply.github.com>
2026-05-23 15:53:10 +0530

    lon

---
+++ longest_seq.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class longest_seq {
+    public int lengthOfLIS(int[] nums) {
+        int n=nums.length;
+        int[] dp=new int[n];
+        Arrays.fill(dp,1);
+        support(0,dp,nums);
+        int max=0;
+        for(int i=0;i<n;i++){
+            max=Math.max(max,dp[i]);
+        }
+        return max;
+    }
+    public void support(int i,int[] dp ,int [] nums){
+        if(i>=nums.length) return;
+        for(int j=0;j<i;j++){
+            if(nums[j]<nums[i])
+                dp[i]=Math.max(dp[j]+1,dp[i]);
+        }
+        support(i+1,dp,nums);
+    }
+    
+}
