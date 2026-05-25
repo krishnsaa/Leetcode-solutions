e2155d74e974fb7cd293fa0173589ee122ba9575
Krishan Pal Singh Sisodiya <153299767+krishnsaa@users.noreply.github.com>
2026-05-25 15:49:56 +0530

    memo

--- product_subarray.java
+++ product_subarray.java
@@ -17,3 +17,49 @@ class product_subarray {
         return ans;
     }
 }
+
+//  with memoization
+
+class Solution {
+
+    HashMap<String, Integer> memo = new HashMap<>();
+    int ans = Integer.MIN_VALUE;
+
+    public int maxProduct(int[] nums) {
+
+        support(nums, 0, 1);
+
+        return ans;
+    }
+
+    public int support(int[] nums, int i, int product) {
+
+        if (i >= nums.length) {
+            return product;
+        }
+
+        String key = i + "#" + product;
+
+        if (memo.containsKey(key)) {
+            return memo.get(key);
+        }
+
+        // continue current subarray
+        int use = product * nums[i];
+
+        ans = Math.max(ans, use);
+
+        int take = support(nums, i + 1, use);
+
+        // start new subarray from current index
+        ans = Math.max(ans, nums[i]);
+
+        int restart = support(nums, i + 1, nums[i]);
+
+        int result = Math.max(take, restart);
+
+        memo.put(key, result);
+
+        return result;
+    }
+}
