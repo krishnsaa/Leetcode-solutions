0890c6ce78a5e40601c900c27811bd4bb6e50f21
Krishan Pal Singh Sisodiya <153299767+krishnsaa@users.noreply.github.com>
2026-05-25 15:49:13 +0530

    dp

---
+++ product_subarray.java
@@ -0,0 +1,19 @@
+class product_subarray {
+    public int maxProduct(int[] nums) {
+        int n=nums.length;
+        int maxprod=nums[0];
+        int minprod=nums[0];
+        int ans=nums[0];
+        for(int i=1;i<n;i++){
+            if(nums[i]<0){
+                int temp=maxprod;
+                maxprod=minprod;
+                minprod=temp;
+            }
+            maxprod=Math.max(nums[i],maxprod*nums[i]);
+            minprod=Math.min(nums[i],minprod * nums[i]);
+            ans=Math.max(ans,maxprod);
+        }
+        return ans;
+    }
+}
