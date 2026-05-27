f86ce1abd1f17734f4474828df8e7bcd9e16b2bc
Krishan Pal Singh Sisodiya <153299767+krishnsaa@users.noreply.github.com>
2026-05-27 14:03:21 +0530

    sp2

---
+++ special_char_2.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class special_char_2 {
+    public int numberOfSpecialChars(String word) {
+        Map<Character,Integer> mpl = new HashMap<>();
+        Map<Character,Integer> mpu = new HashMap<>();
+        for(int i=0;i<word.length();i++){
+            char ch= word.charAt(i);
+            if(Character.isLowerCase(ch)){
+                if(mpl.get(ch)==null){
+                    mpl.put(ch,i);
+                }else{
+                    mpl.put(ch,i);
+                }
+            }else{
+                if(mpu.get(ch)==null){
+                    mpu.put(ch,i);
+                }
+            }
+        }
+        int count=0;
+        for (Map.Entry<Character, Integer> entry : mpl.entrySet()) {
+            char key = entry.getKey();
+            int value = entry.getValue();
+            char keyu=Character.toUpperCase(key);
+            if(mpu.get(keyu)!=null){
+                if(value<mpu.get(keyu)) count++;
+            }
+
+        }
+
+
+        return count;
+        
+    }
+}
