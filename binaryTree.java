f4da286da6f23580acacf5ac4c0fb54b34d95ff0
Krishan Pal Singh Sisodiya <153299767+krishnsaa@users.noreply.github.com>
2026-06-07 13:52:54 +0530

    final

--- binaryTree.java
+++ binaryTree.java
@@ -3,22 +3,8 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-import Leetcode.TreeNode;
 
 public class binaryTree {
-  public class TreeNode {
-      int val;
-      TreeNode left;
-      TreeNode right;
-      TreeNode() {}
-      TreeNode(int val) { this.val = val; }
-      TreeNode(int val, TreeNode left, TreeNode right) {
-          this.val = val;
-          this.left = left;
-          this.right = right;
-      }
-  }
- 
     public TreeNode createBinaryTree(int[][] desc) {
         Map<Integer, int[]> map = new HashMap<>();
         for(int[] dis : desc){
