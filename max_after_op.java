c86d8e203a1f1ba73513921fd2dc6a581abb96e9
Krishan Pal Singh Sisodiya <153299767+krishnsaa@users.noreply.github.com>
2026-06-28 11:22:56 +0530

    max operation

---
+++ max_after_op.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class max_after_op {
+    public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+        Arrays.sort(arr);
+        if(arr.length==0) return 0;
+        if(arr[0]!=1) arr[0]=1;
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]-arr[i-1]>1){
+                arr[i]=arr[i-1]+1;
+            }
+
+        }
+        int max=0;
+        for(int num:arr){
+            max=Math.max(num,max);
+        }
+        return max;
+        
+    }
+}
