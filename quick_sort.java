d5357bcc0c9a6e1a02965925d66344d1eab71296
Krishan Pal Singh Sisodiya <153299767+krishnsaa@users.noreply.github.com>
2026-07-03 19:21:52 +0530

    print

--- quick_sort.java
+++ quick_sort.java
@@ -1,3 +1,15 @@
+import java.util.*;
 public class quick_sort {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String line=sc.nextLine();
+        String[] starr=line.trim().split("\\+s");
+        int[] arr=new int[starr.length];
+        for(int i=0;i<starr.length;i++){
+            arr[i]=Integer.parseInt(starr[i]);
+        }
+        
+        sc.close();
+    }
 
 }
