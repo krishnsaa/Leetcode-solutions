1ecf3f68d45305752a59c9e68b27117a13435e39
Krishan Pal Singh Sisodiya <153299767+krishnsaa@users.noreply.github.com>
2026-07-05 14:37:43 +0530

    dp

--- longest_inc_subseq.java
+++ longest_inc_subseq.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class longest_inc_subseq {
     //BruteForce
@@ -29,4 +32,71 @@ public class longest_inc_subseq {
     //     return true;
     // }
     
+
+    // Memoization on Recursion
+    // public int lengthOfLIS(int[] nums) {
+    //     int n=nums.length;
+    //     int[][] dp=new int[n][n];
+    //     for(int i=0;i<n;i++){
+    //         Arrays.fill(dp[i],-1);
+    //     }
+    //     return findLength(nums,0,-1,dp);
+    // }
+    // int findLength(int[] nums,int i,int prev,int[][] dp){
+    //     if(i==nums.length){
+    //         return 0;
+    //     }
+    //     if(dp[i][prev+1]!=-1) return dp[i][prev+1];
+    //     int len = 0 + findLength(nums,i+1,prev,dp);
+    //     if(prev==-1 || nums[i]>nums[prev]){
+    //         len=Math.max(1+findLength(nums,i+1,i,dp),len);
+    //     }
+    //     return dp[i][prev+1]=len;
+    // }
+    // class Solution {
+//     public int lengthOfLIS(int[] nums) {
+//         int n = nums.length;
+
+//         int[] dp = new int[n];
+//         Arrays.fill(dp, 1);
+
+//         int ans = 1;
+
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < i; j++) {
+
+//                 if (nums[j] < nums[i]) {
+//                     dp[i] = Math.max(dp[i], dp[j] + 1);
+//                 }
+//             }
+
+//             ans = Math.max(ans, dp[i]);
+//         }
+
+//         return ans;
+//     }
+// }
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        List<Integer> lis = new ArrayList<>();
+
+        for (int num : nums) {
+
+            int idx = Collections.binarySearch(lis, num);
+
+            if (idx < 0)
+                idx = -(idx + 1);
+
+            if (idx == lis.size())
+                lis.add(num);
+            else
+                lis.set(idx, num);
+        }
+
+        return lis.size();
+    }
+}
+
+
+
 }
